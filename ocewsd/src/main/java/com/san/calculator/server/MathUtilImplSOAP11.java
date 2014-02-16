97fe02c5c313c7accd01e2b7ed41d66bf5d6968a
Aragaki <Aragaki@62ae4cac-d60a-4a85-9c9f-b9abfb254e86>
2014-02-16 14:37:53 +0000

    delete

--- ocewsd/src/main/java/com/san/calculator/server/MathUtilImplSOAP11.java
+++ ocewsd/src/main/java/com/san/calculator/server/MathUtilImplSOAP11.java
@@ -1,11 +1,14 @@
 package com.san.calculator.server;
 
+import javax.annotation.security.RolesAllowed;
 import javax.jws.WebService;
 
 @WebService(endpointInterface = "com.san.calculator.server.MathUtil", targetNamespace = "http://www.san.com/calculator",
         serviceName = "Calculator", portName = "CalculatorTestPortName11")
-public class MathUtilImplSOAP11 {
+public class MathUtilImplSOAP11 implements MathUtil {
 
+    @Override
+    @RolesAllowed("basicUser")
     public int addNumbers(int arg0, int arg1) {
         return arg0 + arg1;
     }
--- ocewsd/src/main/java/com/san/calculator/server/MathUtilImplSOAP12.java
+++ ocewsd/src/main/java/com/san/calculator/server/MathUtilImplSOAP12.java
@@ -3,9 +3,10 @@ package com.san.calculator.server;
 import javax.jws.WebService;
 
 @WebService(endpointInterface = "com.san.calculator.server.MathUtil", targetNamespace = "http://www.san.com/calculator",
-        serviceName = "Calculator", portName = "CalculatorTestPortName12")
-public class MathUtilImplSOAP12 {
+        serviceName = "Calculator12", portName = "CalculatorTestPortName12")
+public class MathUtilImplSOAP12 implements MathUtil {
 
+    @Override
     public int addNumbers(int arg0, int arg1) {
         return arg0 + arg1;
     }
